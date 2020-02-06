5655d92d99aa8635a5df29c6f3fe893962c89e6b
Antonio Ramirez <sepehralizade@live.com>
2020-02-06 13:30:35 +0300

    concat and minified

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
-var { src, series, dest, watch } = require('gulp');
+var { src, series, dest, watch, concat } = require('gulp');
 var stylus = require('gulp-stylus');
 var cssc = require('gulp-css-condense');
 var sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
+var concatCss = require('gulp-concat-css');
 
 exports.vendor = () => {
   return src('./compressed/**')
     .pipe(postcss([autoprefixer()]))
     .pipe(dest('./vendor'));
+};
+
+exports.final = () => {
+  return src('./vendor/**')
+    .pipe(concatCss('styles.min.css'))
+    .pipe(cssc())
+    .pipe(dest('./final'));
 };
 
 exports.compress = () => {
@@ -25,7 +33,7 @@ exports.stylusTask = () => {
     .pipe(dest('./css'));
 };
 
-exports.defaultTask = series([this.stylusTask, this.compress, this.vendor]);
+exports.defaultTask = series([this.stylusTask, this.compress, this.vendor, this.final]);
 
 watchTask = () => {
   return watch('./stylus/**', { ignoreInitial: false }, this.defaultTask);
